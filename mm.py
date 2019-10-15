346302f861396d9a02c926ecc3a4d67e1b7ab0fb
Anjali-hub <56326140+Anjali-hub@users.noreply.github.com>
2019-10-15 17:55:59 -0400

    Delete mm.py

--- mm.py
+++
@@ -1,9 +0,0 @@
-import sys
-print(sys.argv)
-file_name = sys.argv('airbnb/first_10.csv')
-f = open(file_name)
-lines = f.readlines()
-for line in lines[1:]:
-    columns = line.split(',')
-    price = columns[9]
-    print(price)
